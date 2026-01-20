eb75b1125d7e9c1693d3d73dd50c0d989982b721
Gantt-Sucessor <jogoperdi@gmail.com>
2026-01-20 13:45:21 -0400

    Subindo esquema básico de pastas e arquivos para Início

---
+++ main.py
@@ -0,0 +1,19 @@
+#INICIAR ULTRON
+
+#carregar identidade
+#carregar memória
+
+#ENQUANTO Ultron estiver ativo:
+#    receber entrada do usuário
+#    interpretar entrada (percepção)
+#    decidir o que fazer (decisão)
+#    executar resposta ou ação
+#    registrar resultado na memória
+
+#ENCERRAR ULTRON
+
+#SE entrada contém "cansado":
+#    resposta = sugerir pausa
+#SENÃO:
+#    resposta = sugerir continuar
+
